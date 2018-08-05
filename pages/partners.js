e73dff7938b91d460bb7cdeac9acc4ed88579296
Roman Timashev <roman@tmshv.ru>
2018-08-06 02:34:58 +0300

    partneres updated

--- pages/partners.js
+++ pages/partners.js
@@ -4,30 +4,38 @@ import Title from '../components/Title';
 import TextBlock from '../components/TextBlock';
 import Article from '../components/Article';
 
+const Partner = ({ href, title, src }) => (
+    <p className="logo-partners">
+        <a href={href} target="_blank" title={title}>
+            <img src={src} height="22px" />
+        </a>
+    </p>
+)
+
 export default () => (
     <DefaultLayout>
         <Title>ПАРТНЕРЫ</Title>
 
         <Article>
             <TextBlock>
-                <div className="logo-partners">
-                    <a href="http://www.mlaplus.com/" target="_blank" title="www.mlaplus.com">
-                        <img src="/static/logo_mla+.svg" height="22px" />
-                    </a>
-                </div>
+                <Partner
+                    href='http://www.mlaplus.com/'
+                    title='www.mlaplus.com'
+                    src='/static/logo_mla+.svg'
+                />
 
-                <div className="logo-partners">
-                    <a href="http://www.knappt.com/" target="_blank" title="www.knappt.com">
-                        <img src="/static/logo_knappt.svg" height="30px" />
-                    </a>
-                </div>
+                <Partner
+                    href='http://www.knappt.com/'
+                    title='www.knappt.com'
+                    src='/static/logo_knappt.svg'
+                />
 
-                <div className="logo-partners">
-                    <a href="https://vk.com/divometallspb" target="_blank" title="vk.com/divometallspb">
-                        <img src="/static/logo_divo_metall.svg" height="30px" />
-                    </a>
-                </div>
-            </TextBlock>
+                <Partner
+                    href='https://vk.com/divometallspb'
+                    title='vk.com/divometallspb'
+                    src='/static/logo_divo_metall.svg'
+                />
+           </TextBlock>
         </Article>
-    </DefaultLayout>
+    </DefaultLayout >
 )
