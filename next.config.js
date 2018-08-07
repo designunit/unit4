5afccfcecd5341e57e6296b029ed3957de357ea1
Roman Timashev <roman@tmshv.ru>
2018-08-07 23:33:24 +0300

    next config updated

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 const withLess = require('@zeit/next-less')
 
 const production = process.env.NODE_ENV === 'production';
-const repo = 'designunit4'
 
 module.exports = withLess({
-	assetPrefix: production
-		? `/${repo}`
-		: '',
+	assetPrefix: '',
 })
