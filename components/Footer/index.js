d25e3310aa0e5204c0fa00570c9091d2479e2789
Roman Timashev <roman@tmshv.ru>
2018-08-09 00:27:08 +0300

    footer updated. style updated

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,17 @@
-import react from 'react'
+import Contact from '../Contact'
 
 import './styles.less'
 
 export default () => (
     <footer>
         <p className="copyright">© 2018 design unit 4</p>
+        <p>
+            <Contact
+                href="mailto:inbox@unit4.io"
+                mix="light"
+            >
+                inbox@unit4.io
+            </Contact>
+        </p>
     </footer>
 )
