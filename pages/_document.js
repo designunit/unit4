fc0d1eec4edce4ce85d4e041251cc95b7e3ad3f4
Roman Timashev <roman@tmshv.ru>
2018-08-08 23:04:21 +0300

    fonts updated

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
     return (
       <html lang='ru'>
         <Head>
+          <link rel="stylesheet" href="/static/style.css" />
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link rel="icon" href="/static/favicon1.png" type="image/x-icon" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
