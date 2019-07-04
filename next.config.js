e50c925d0a089e3f1262b611a06bc5cd2df5c2b5
Roman Timashev <roman@tmshv.ru>
2019-07-04 19:53:25 +0300

    added MDX support

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const withLess = require('@zeit/next-less')
+const withMdx = require('@next/mdx')()
 
 const production = process.env.NODE_ENV === 'production';
 
-module.exports = withLess({
-	assetPrefix: '',
-})
+module.exports = withMdx(withLess({
+    assetPrefix: '',
+    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+}))
