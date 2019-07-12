c5df0b7b6b34e30b86a133da73e10a0b849955dd
Roman Timashev <roman@tmshv.ru>
2019-07-12 19:13:03 +0300

    Title component moved to typescript + rm less

---
+++ components/Title/index.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import Head from 'next/head'
+
+export interface ITitleProps {
+    caption?: string
+}
+
+export const Title: React.FC<ITitleProps> = ({caption, children}) => (
+    <div className={'title'}>
+        <style jsx>{`
+            .title {
+                margin-bottom: 50px;
+                padding: var(--content-padding);
+            }
+
+            h1 {
+                margin: 0;
+
+                font-family: var(--font-main-family);
+                font-size: var(--font-main-size);
+            }
+
+            .title-caption {
+                margin: 0;
+
+                font-family: var(--font-second-family);
+                line-height: var(--font-second-size);
+            }
+        `}</style>
+
+        <Head>
+            <title>{children}</title>
+        </Head>
+
+        <h1>
+            {children}
+        </h1>
+        
+        <p className={'title-caption'}>
+            {caption}
+        </p>
+    </div>
+)
+
+export default Title
