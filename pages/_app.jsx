799d384f532351f6031fbb929c22f9fde26644b5
Roman Timashev <roman@tmshv.ru>
2019-07-12 17:37:31 +0300

    next app update to handle mdx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import App, { Container } from 'next/app'
 import DefaultLayout from '../components/DefaultLayout'
+import Article from '../components/Article'
 
 export default class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props
+        const isMdx = Component.hasOwnProperty('isMDXComponent') ? Component.isMDXComponent : false
+        const content = isMdx ? (
+            <Article>
+                <Component {...pageProps}/>
+            </Article>
+        ) : (
+            <Component {...pageProps}/>
+        )
 
         return (
             <Container>
                 <DefaultLayout>
-                    <Component {...pageProps} />
+                    {content}
                 </DefaultLayout >
             </Container>
         )
