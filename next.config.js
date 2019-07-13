af615e17b3cd56d8833816c5e2c8c0bf5aa9f99c
Roman Timashev <roman@tmshv.ru>
2019-07-13 22:17:58 +0300

    added next-css

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
+const withCss = require('@zeit/next-css')
 const withLess = require('@zeit/next-less')
 const withMdx = require('@next/mdx')()
 
 const production = process.env.NODE_ENV === 'production';
 
-module.exports = withMdx(withLess({
+module.exports = withMdx(withCss(withLess({
     assetPrefix: '',
     pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-}))
+})))
