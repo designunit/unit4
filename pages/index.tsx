3bf4fe17700307ed20b9fe73c10aef70b5d90710
Roman Timashev <roman@tmshv.ru>
2019-08-23 20:54:11 +0300

    Added gallery to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next'
 import dynamic from 'next/dynamic'
 import * as React from 'react'
+import { Gallery } from '../src/components/Gallery'
 
 const UnitCanvasDynamic = dynamic(() => import('../src/components/UnitCanvas'), {
     ssr: false,
@@ -8,7 +9,55 @@ const UnitCanvasDynamic = dynamic(() => import('../src/components/UnitCanvas'), 
 
 const Page: NextPage = () => (
     <>
-        <UnitCanvasDynamic/>
+        <UnitCanvasDynamic />
+
+        <section>
+            <style jsx>{`
+                section {
+                    display: flex;
+                    justify-content: center;
+
+                    margin: 4em 0 2em;
+                }
+
+                div {
+                    width: 60%;
+                }
+            `}</style>
+
+            <div>
+                <Gallery
+                    minCellSize={150}
+                    gridGap={20}
+                    smallLabel={true}
+                    items={[
+                        {
+                            href: '/repository/hovrinka',
+                            src: 'https://unit4.io/files/hovrinka/hovrinka.jpg',
+                            text: (
+                                <>
+                                    СОЦИАЛЬНО-КУЛЬТУРНЫЙ
+                                    <br />
+                                    ЦЕНТР
+                                    <br />
+                                    ХОВРИНКА
+                                </>
+                            ),
+                        },
+                        {
+                            href: '/repository/delta',
+                            src: 'https://unit4.io/files/delta/delta.jpg',
+                            text: 'БЛАГОУСТРОЙСТВО ЖК COMCITY МОСКВА',
+                        },
+                        {
+                            href: '/repository/gor_projects_spb',
+                            src: 'https://spb.city4people.ru/static/img_c4p/project3/todo-after_.jpg',
+                            text: 'КОНЦЕПЦИИ spb.city4people.ru',
+                        },
+                    ]}
+                />
+            </div>
+        </section>
     </>
 )
 
