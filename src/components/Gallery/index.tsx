e82044765d9a5a378edd5c472b80a27b4538aa98
Roman Timashev <roman@tmshv.ru>
2019-08-23 21:15:59 +0300

    GalleryItem moved to separate file

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,15 +1,11 @@
-import className from 'classnames'
 import * as React from 'react'
+import { GalleryItem } from './GalleryItem'
 
 export interface IGalleryItem {
     src: string
     alt?: string
     href?: string
     text?: string | React.ReactNode
-}
-
-export interface IGalleryItemProps extends IGalleryItem {
-    smallLabel: boolean
 }
 
 export interface IGallery {
@@ -19,104 +15,6 @@ export interface IGallery {
     smallLabel?: boolean
     gridGap?: number
 }
-
-const GalleryItem: React.FC<IGalleryItemProps> = props => (
-    <a
-        className={'image'}
-        href={props.href}
-    >
-        <style jsx>{`
-            a {
-                display: block;
-
-                overflow: hidden;
-            }
-
-            a:hover img {
-                transform: scale(1);
-            }
-
-            img {
-                display: block;
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-
-                filter: sepia(100%) hue-rotate(260deg);
-                filter: grayscale(100%) contrast(0.75) brightness(1.25);
-
-                transition: 0.25s ease;
-                transform: scale(1);
-            }
-
-            .image {
-                position: relative;
-            }
-
-            .image::after {
-                content: '';
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-
-                transition: .5s ease;
-
-                background-color: #f91b86;
-                mix-blend-mode: multiply;
-            }
-
-            .label {
-                z-index: 1;
-                pointer-events: none;
-
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-
-                display: flex;
-                justify-content: center;
-                align-items: center;
-
-                color: white;
-                font-family: var(--font-normal-family);
-                font-weight: bolder;
-                font-size: 1.2em;
-                text-align: center;
-
-                padding: 5px;
-            }
-
-            .label.small {
-                font-size: 0.8em;
-                line-height: 1.6em;
-            }
-
-            @media screen and (max-width: 31.25em) {
-                .label {
-                    font-size: 0.8em;
-                }
-
-                img {
-                    transform: scale(1.1);
-                }
-            }
-        `}</style>
-
-        <img
-            src={props.src}
-        />
-
-        <div className={className('label', {
-            small: props.smallLabel,
-        })}>
-            {props.text}
-        </div>
-    </a>
-)
 
 export const Gallery: React.FC<IGallery> = ({ gridGap = 10, minCellSize = 200, smallLabel = false, ...props }) => (
     <div
