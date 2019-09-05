0c4eeea85d655322f3766cc58e52c0a243f70502
Roman Timashev <roman@tmshv.ru>
2019-09-05 18:20:31 +0300

    added WideBlock component for full width content

---
+++ src/components/WideBlock/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+
+export interface IWideBlockProps {
+    style?: React.CSSProperties
+}
+
+export const WideBlock: React.FC<IWideBlockProps> = props => (
+    <div style={props.style}>
+        <style jsx>{`
+            div {
+                position: relative;
+                width: 100vw;
+                margin-left: -50vw;
+                left: 50%;
+            }
+        `}</style>
+
+        {props.children}
+    </div>
+)
