ef55cc01e97dd6b0f1071fc548e95f71e1d106e4
Roman Timashev <roman@tmshv.ru>
2019-09-06 18:57:21 +0300

    added highlight to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next'
 import dynamic from 'next/dynamic'
 import * as React from 'react'
+import { UnitHighlight } from '../src/components/UnitHighlight'
 
 const UnitCanvasDynamic = dynamic(() => import('../src/components/UnitCanvas'), {
     ssr: false,
@@ -14,6 +15,29 @@ const Page: NextPage = () => (
                 marginTop: 50,
             }}
         />
+
+        <UnitHighlight items={[
+            {
+                date: new Date('2019-08-30'),
+                href: '/repository/oymyakon',
+                title: 'Оймякон : : п(о)люс со-временности',
+            },
+            {
+                date: new Date('2019-08-25'),
+                href: '/repository/gor_projects_spb',
+                title: 'Концепции spb.city4people.ru',
+            },
+            {
+                date: new Date('2019-08-12'),
+                href: '/repository/hovrinka',
+                title: 'Социально-культурный центр Ховринка',
+            },
+            {
+                date: new Date('2019-07-28'),
+                href: '/repository/delta',
+                title: 'Благоустройство ЖК ComCity',
+            },
+        ]} />
     </>
 )
 
