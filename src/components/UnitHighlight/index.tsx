32bf4f5f19f066756129d01de840338ec27378d8
Roman Timashev <roman@tmshv.ru>
2019-09-06 18:47:54 +0300

    added UnitHighlight component

---
+++ src/components/UnitHighlight/index.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react'
+
+export interface IUnitHighlightProps {
+    style?: React.CSSProperties
+    newWindow?: boolean
+    items: Array<{
+        date?: Date,
+        url: string,
+        title: string,
+    }>
+}
+
+export const UnitHighlight: React.FC<IUnitHighlightProps> = ({ newWindow = false, ...props }) => {
+    const dateFormatterRef = React.useRef(new Intl.DateTimeFormat('ru-RU'))
+
+    return (
+        <section style={props.style}>
+            <style jsx>{`
+                ul {
+                    padding-left: 0;
+                }
+
+                li {
+                    display: flex;
+                }
+
+                .date {
+                    padding-right: 1em;
+                }
+
+                .link {
+                    flex: 1;
+                }
+
+                a:link, a:visited {
+                    background-color:  #ff0066;
+                    color: white;
+                    text-decoration: none;
+                    padding: 0 8px;
+                }
+
+                a:hover, a:active {
+                    background-color: black;
+                }
+            `}</style>
+
+            <ul>
+                {props.items.map(x => (
+                    <li key={x.url}>
+                        {!x.date ? null : (
+                            <span className={'date'}>
+                                {dateFormatterRef.current.format(x.date)}
+                            </span>
+                        )}
+                        <span className={'link'}>
+                            <a
+                                href={x.url}
+                                target={newWindow ? '_blank' : null}
+                            >
+                                {x.title}
+                            </a>
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    )
+}
