70215517bc5047a1b722f3dfed80072d0d1a244a
Roman Timashev <roman@tmshv.ru>
2019-11-23 01:05:42 +0300

    server cleanup

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import next from 'next'
 import nextI18NextMiddleware from 'next-i18next/middleware'
-import { parse } from 'url'
 import nextI18next from '../i18n'
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -23,42 +22,6 @@ app.prepare()
         const server = express()
 
         server.use(nextI18NextMiddleware(nextI18next))
-
-        // server.get('/projects', (req, res) => {
-        //     const actualPage = '/projects'
-        //     const queryParams = {
-        //         city: 'all',
-        //     }
-
-        //     app.render(req, res, actualPage, queryParams)
-        // })
-
-        // server.get('/projects/:city', (req, res) => {
-        //     const actualPage = '/projects'
-        //     const queryParams = {
-        //         city: req.params.city,
-        //     }
-
-        //     app.render(req, res, actualPage, queryParams)
-        // })
-
-        // server.get('/project/:slug', (req, res) => {
-        //     const actualPage = '/project'
-        //     const queryParams = {
-        //         slug: req.params.slug,
-        //     }
-
-        //     app.render(req, res, actualPage, queryParams)
-        // })
-
-        // server.get('/event/:slug', (req, res) => {
-        //     const actualPage = '/event'
-        //     const queryParams = {
-        //         slug: req.params.slug,
-        //     }
-
-        //     app.render(req, res, actualPage, queryParams)
-        // })
 
         server.get('/', (req, res) => {
             app.render(req, res, '/', {})
@@ -88,36 +51,13 @@ app.prepare()
 
         server.get('/repository/:slug', (req, res) => {
             const lang = (req as any).lng
-
             const slug = req.params.slug
             const actualPage = createPageName(`/repository/${slug}`, defaultLang, lang)
-            const queryParams = {
-                //     slug: req.params.slug,
-            }
 
-            console.log('handle repository page', req.url, slug, actualPage, lang, req.query)
-
-            app.render(req, res, actualPage, queryParams)
+            app.render(req, res, actualPage, {})
         })
 
         server.get('*', (req, res) => {
-            const lang = (req as any).lng
-            console.log('handle request with next', req.url, lang)
-
-            const parsedUrl = parse(req.url, true)
-            const { pathname, query } = parsedUrl
-
-            // if (pathname) {
-            // }
-
-            // if (pathname === '/a') {
-            //     app.render(req, res, '/b', query)
-            // } else if (pathname === '/b') {
-            //     app.render(req, res, '/a', query)
-            // } else {
-            //     handle(req, res, parsedUrl)
-            // }
-
             return handle(req, res)
         })
 
