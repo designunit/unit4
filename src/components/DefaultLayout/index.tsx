bed91365983c8c222b5006e65e31539a60ea4afe
Roman Timashev <roman@tmshv.ru>
2019-11-22 23:44:40 +0300

    menu translations

--- src/components/DefaultLayout/index.tsx
+++ src/components/DefaultLayout/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 
 import className from 'classnames'
 import Head from 'next/head'
+import { useTranslation } from '../../i18n'
 import { Footer } from '../Footer'
 import { Logo } from '../Logo'
 import { Menu } from '../Menu'
@@ -14,49 +15,53 @@ export interface IDefaultLayoutProps {
     title: string
 }
 
-export const DefaultLayout: React.FC<IDefaultLayoutProps> = props => (
-    <div className={className('layout', {
-        'layout--center': false,
-    })}>
-        <Head>
-            <title>{props.title}</title>
-        </Head>
+export const DefaultLayout: React.FC<IDefaultLayoutProps> = props => {
+    const { t } = useTranslation()
 
-        <div className='content'>
-            {!props.showHeader ? null : (
-                <header>
-                    <Logo
-                        href={'/'}
-                    />
+    return (
+        <div className={className('layout', {
+            'layout--center': false,
+        })}>
+            <Head>
+                <title>{props.title}</title>
+            </Head>
 
-                    <Menu vertical
-                        items={[
-                            {
-                                href: '/about',
-                                text: 'о нас',
-                            },
-                            {
-                                href: '/repository',
-                                text: 'репозиторий',
-                            },
-                            {
-                                href: '/partners',
-                                text: 'партнеры',
-                            },
-                            {
-                                href: '/contact',
-                                text: 'контакты',
-                            },
-                        ]}
-                    />
-                </header>
-            )}
+            <div className='content'>
+                {!props.showHeader ? null : (
+                    <header>
+                        <Logo
+                            href={'/'}
+                        />
 
-            <main>
-                {props.children}
-            </main>
+                        <Menu vertical
+                            items={[
+                                {
+                                    href: '/about',
+                                    text: t('menu-about'),
+                                },
+                                {
+                                    href: '/repository',
+                                    text: t('menu-repository'),
+                                },
+                                {
+                                    href: '/partners',
+                                    text: t('menu-partners'),
+                                },
+                                {
+                                    href: '/contact',
+                                    text: t('menu-contacts'),
+                                },
+                            ]}
+                        />
+                    </header>
+                )}
 
-            <Footer />
+                <main>
+                    {props.children}
+                </main>
+
+                <Footer />
+            </div>
         </div>
-    </div>
-)
+    )
+}
