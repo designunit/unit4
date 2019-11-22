0b22ef6c5479bf9af7ce96cf8bb9159728eaf69f
Roman Timashev <roman@tmshv.ru>
2019-11-22 23:03:27 +0300

    repository titles doesnt use html

--- pages/repository.tsx
+++ pages/repository.tsx
@@ -21,47 +21,27 @@ const Page: NextPage = () => {
                     {
                         href: '/repository/edu_ugra',
                         src: 'https://unit4.io/files/edu_ugra/01.jpg',
-                        text: (
-                            <span>
-                                ОБУЧАЮЩИЙ СЕМИНАР ХМАО ФКГС
-                            </span>
-                        ),
+                        text: 'ОБУЧАЮЩИЙ СЕМИНАР ХМАО ФКГС',
                     },
                     {
                         href: '/repository/uray_ppi',
                         src: 'https://unit4.io/files/uray_ppi/urai.jpg',
-                        text: (
-                            <span>
-                                ИССЛЕДОВАНИЕ УРАЙ ХМАО
-                            </span>
-                        ),
+                        text: 'ИССЛЕДОВАНИЕ УРАЙ ХМАО',
                     },
                     {
                         href: '/repository/heterotopia',
                         src: 'https://unit4.io/files/heterotopia/heterotopia.gif',
-                        text: (
-                            <span>
-                                ГЕТЕРОТОПИИ СО-ВРЕМЕННОСТИ
-                            </span>
-                        ),
+                        text: 'ГЕТЕРОТОПИИ СО-ВРЕМЕННОСТИ',
                     },
                     {
                         href: '/repository/oymyakon',
                         src: 'https://unit4.io/files/oy/viz_gulag.jpg',
-                        text: (
-                            <span>
-                                ОЙМЯКОН п(о)люс со-временности
-                            </span>
-                        ),
+                        text: 'ОЙМЯКОН п(о)люс со-временности',
                     },
                     {
                         href: '/repository/hovrinka',
                         src: 'https://unit4.io/files/hovrinka/hovrinka.jpg',
-                        text: (
-                            <span>
-                                СОЦИАЛЬНО-КУЛЬТУРНЫЙ ЦЕНТР ХОВРИНКА
-                            </span>
-                        ),
+                        text: 'СОЦИАЛЬНО-КУЛЬТУРНЫЙ ЦЕНТР ХОВРИНКА',
                     },
                     {
                         href: '/repository/delta',
@@ -71,11 +51,7 @@ const Page: NextPage = () => {
                     {
                         href: '/repository/application_manual',
                         src: 'https://unit4.io/files/app_manual/manual.jpg',
-                        text: (
-                            <span>
-                                МЕТОДИЧЕСКИЕ РЕКОМЕНДАЦИИ ХМАО
-                            </span>
-                        ),
+                        text: 'МЕТОДИЧЕСКИЕ РЕКОМЕНДАЦИИ ХМАО',
                     },
                     {
                         href: '/repository/gor_projects_spb',
@@ -85,29 +61,17 @@ const Page: NextPage = () => {
                     {
                         href: '/repository/yoshkola',
                         src: 'https://unit4.io/files/yoshkola/04.jpg',
-                        text: (
-                            <span>
-                                IT ШКОЛА ЙОШКАР-ОЛА
-                            </span>
-                        ),
+                        text: 'IT ШКОЛА ЙОШКАР-ОЛА',
                     },
                     {
                         href: '/repository/boulevard_scnd',
                         src: 'https://unit4.io/files/boulevard_scnd/boulevard.jpg',
-                        text: (
-                            <span>
-                                БУЛЬВАР ЖИЛОГО РАЙОНА СКАНДИНАВИЯ
-                            </span>
-                        ),
+                        text: 'БУЛЬВАР ЖИЛОГО РАЙОНА СКАНДИНАВИЯ',
                     },
                     {
                         href: '/repository/park_scnd',
                         src: 'https://unit4.io/files/park_scnd/park.jpg',
-                        text: (
-                            <span>
-                                ПАРК ЖИЛОГО РАЙОНА СКАНДИНАВИЯ
-                            </span>
-                        ),
+                        text: 'ПАРК ЖИЛОГО РАЙОНА СКАНДИНАВИЯ',
                     },
                     {
                         href: '/repository/shelter',
@@ -137,62 +101,42 @@ const Page: NextPage = () => {
                     {
                         href: '/repository/garagescreen',
                         src: 'https://unit4.io/files/garagescreen/02.gif',
-                        text: (
-                            <span>
-                                GARAGE SCREEN COMPETITION
-                            </span>
-                        ),
+                        text: 'GARAGE SCREEN COMPETITION',
                     },
                     {
                         href: '/repository/d_scnd',
                         src: 'https://unit4.io/files/d_scnd/03.jpg',
-                        text: (
-                            <span>
-                                ДВОРЫ ЖИЛОГО РАЙОНА СКАНДИНАВИЯ
-                            </span>
-                        ),
+                        text: 'ДВОРЫ ЖИЛОГО РАЙОНА СКАНДИНАВИЯ',
                     },
                     {
-                        href: '/repository/krvostok',
+                        href:  '/repository/krvostok',
                         src: 'https://unit4.io/files/krvostok/01.jpg',
-                        text: (
-                            <span>
-                                КОНЦЕПЦИЯ БЛАГОУСТРОЙСТВА ЗАВОДА КРАСНЫЙ ВОСТОК
-                            </span>
-                        ),
+                        text: 'КОНЦЕПЦИЯ БЛАГОУСТРОЙСТВА ЗАВОДА КРАСНЫЙ ВОСТОК',
                     },
                     {
-                        href: '/repository/trollgardens',
+                        href:  '/repository/trollgardens',
                         src: 'https://unit4.io/files/trollgardens/05.jpg',
                         text: 'САДЫ ТРОЛЛЕЙ',
                     },
                     {
-                        href: '/repository/chistopol2',
+                        href:  '/repository/chistopol2',
                         src: 'https://unit4.io/files/chistopol2/07.jpg',
-                        text: (
-                            <span>
-                                БУЛЬВАР ЧИСТОПОЛЬ ТАТАРСТАН ФАЗА #2
-                            </span>
-                        ),
+                        text: 'БУЛЬВАР ЧИСТОПОЛЬ ТАТАРСТАН ФАЗА #2',
                     },
                     {
-                        href: '/repository/kemb',
+                        href:  '/repository/kemb',
                         src: 'https://unit4.io/files/kemb/01.jpg',
                         text: 'ИССЛЕДОВАНИЕ НАБЕРЕЖНОЙ КАРПОВКИ',
                     },
                     {
-                        href: '/repository/model4',
+                        href:  '/repository/model4',
                         src: 'https://unit4.io/files/model4/01.jpg',
                         text: 'СЕРИЯ ИЗДЕЛИЙ МОДЕЛЬ : : 4',
                     },
                     {
-                        href: '/repository/chistopol',
+                        href:  '/repository/chistopol',
                         src: 'https://unit4.io/files/chistopol/02.jpg',
-                        text: (
-                            <span>
-                                БУЛЬВАР ЧИСТОПОЛЬ ТАТАРСТАН ФАЗА #1
-                            </span>
-                        ),
+                        text: 'БУЛЬВАР ЧИСТОПОЛЬ ТАТАРСТАН ФАЗА #1',
                     },
                 ]}
             />
