1a2520705655e84655af1a01490e558625074f77
Roman Timashev <roman@tmshv.ru>
2019-11-23 00:25:01 +0300

    repository project links fix

--- pages/repository.tsx
+++ pages/repository.tsx
@@ -4,10 +4,12 @@ import { NextPage } from 'next'
 
 import { Gallery } from '../src/components/Gallery'
 import { Title } from '../src/components/Title'
+import { useLangUrlPrefix } from '../src/hooks/useLangUrlPrefix'
 import { useTranslation, withTranslation } from '../src/i18n'
 
 const Page: NextPage = () => {
     const { t } = useTranslation()
+    const lang = useLangUrlPrefix()
 
     return (
         <>
@@ -19,122 +21,122 @@ const Page: NextPage = () => {
                 }}
                 items={[
                     {
-                        href: '/repository/edu_ugra',
+                        href: t(`${lang}/repository/edu_ugra`),
                         src: 'https://unit4.io/files/edu_ugra/01.jpg',
                         text: t('repository-edu_ugra'),
                     },
                     {
-                        href: '/repository/uray_ppi',
+                        href: t(`${lang}/repository/uray_ppi`),
                         src: 'https://unit4.io/files/uray_ppi/urai.jpg',
                         text: t('repository-uray_ppi'),
                     },
                     {
-                        href: '/repository/heterotopia',
+                        href: t(`${lang}/heterotopia`),
                         src: 'https://unit4.io/files/heterotopia/heterotopia.gif',
                         text: t('repository-heterotopia'),
                     },
                     {
-                        href: '/repository/oymyakon',
+                        href: t(`${lang}/oymyakon`),
                         src: 'https://unit4.io/files/oy/viz_gulag.jpg',
                         text: t('repository-oymyakon'),
                     },
                     {
-                        href: '/repository/hovrinka',
+                        href: t(`${lang}/hovrinka`),
                         src: 'https://unit4.io/files/hovrinka/hovrinka.jpg',
                         text: t('repository-hovrinka'),
                     },
                     {
-                        href: '/repository/delta',
+                        href: t(`${lang}/delta`),
                         src: 'https://unit4.io/files/delta/delta.jpg',
                         text: t('repository-delta'),
                     },
                     {
-                        href: '/repository/application_manual',
+                        href: t(`${lang}/application_manual`),
                         src: 'https://unit4.io/files/app_manual/manual.jpg',
                         text: t('repository-application_manual'),
                     },
                     {
-                        href: '/repository/gor_projects_spb',
+                        href: t(`${lang}/gor_projects_spb`),
                         src: 'https://spb.city4people.ru/static/img_c4p/project3/todo-after_.jpg',
                         text: t('repository-gor_projects_spb'),
                     },
                     {
-                        href: '/repository/yoshkola',
+                        href: t(`${lang}/yoshkola`),
                         src: 'https://unit4.io/files/yoshkola/04.jpg',
                         text: t('repository-yoshkola'),
                     },
                     {
-                        href: '/repository/boulevard_scnd',
+                        href: t(`${lang}/boulevard_scnd`),
                         src: 'https://unit4.io/files/boulevard_scnd/boulevard.jpg',
                         text: t('repository-boulevard_scnd'),
                     },
                     {
-                        href: '/repository/park_scnd',
+                        href: t(`${lang}/park_scnd`),
                         src: 'https://unit4.io/files/park_scnd/park.jpg',
                         text: t('repository-park_scnd'),
                     },
                     {
-                        href: '/repository/shelter',
+                        href: t(`${lang}/shelter`),
                         src: 'https://unit4.io/files/shelter/01.jpg',
                         text: t('repository-shelter'),
                     },
                     {
-                        href: '/repository/swarm',
+                        href: t(`${lang}/swarm`),
                         src: 'https://unit4.io/files/swarm/swarm.jpg',
                         text: t('repository-swarm'),
                     },
                     {
-                        href: '/repository/dc_scnd',
+                        href: t(`${lang}/dc_scnd`),
                         src: 'https://unit4.io/files/dc_scnd/dc.gif',
                         text: t('repository-dc_scnd'),
                     },
                     {
-                        href: '/repository/gb_scnd',
+                        href: t(`${lang}/gb_scnd`),
                         src: 'https://unit4.io/files/gb_scnd/gb.jpg',
                         text: t('repository-gb_scnd'),
                     },
                     {
-                        href: '/repository/samarapark',
+                        href: t(`${lang}/samarapark`),
                         src: 'https://unit4.io/files/samarapark/10.jpg',
                         text: t('repository-samarapark'),
                     },
                     {
-                        href: '/repository/garagescreen',
+                        href: t(`${lang}/garagescreen`),
                         src: 'https://unit4.io/files/garagescreen/02.gif',
                         text: t('repository-garagescreen'),
                     },
                     {
-                        href: '/repository/d_scnd',
+                        href: t(`${lang}/repository/d_scnd`),
                         src: 'https://unit4.io/files/d_scnd/03.jpg',
                         text: t('repository-d_scnd'),
                     },
                     {
-                        href:  '/repository/krvostok',
+                        href: t(`${lang}/krvostok`),
                         src: 'https://unit4.io/files/krvostok/01.jpg',
                         text: t('repository-krvostok'),
                     },
                     {
-                        href:  '/repository/trollgardens',
+                        href: t(`${lang}/trollgardens`),
                         src: 'https://unit4.io/files/trollgardens/05.jpg',
                         text: t('repository-trollgardens'),
                     },
                     {
-                        href:  '/repository/chistopol2',
+                        href: t(`${lang}/chistopol2`),
                         src: 'https://unit4.io/files/chistopol2/07.jpg',
                         text: t('repository-chistopol2'),
                     },
                     {
-                        href:  '/repository/kemb',
+                        href: t(`${lang}/kemb`),
                         src: 'https://unit4.io/files/kemb/01.jpg',
                         text: t('repository-kemb'),
                     },
                     {
-                        href:  '/repository/model4',
+                        href: t(`${lang}/model4`),
                         src: 'https://unit4.io/files/model4/01.jpg',
                         text: t('repository-model4'),
                     },
                     {
-                        href:  '/repository/chistopol',
+                        href: t(`${lang}/chistopol`),
                         src: 'https://unit4.io/files/chistopol/02.jpg',
                         text: t('repository-chistopol'),
                     },
