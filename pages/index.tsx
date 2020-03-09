737365c0535ced89e3951b115e129262c8af4a4f
Roman Timashev <roman@tmshv.ru>
2020-03-09 04:29:22 +0300

    pages slug changed; added redirect from old paths

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,154 @@
-import { NextPage } from 'next'
-import dynamic from 'next/dynamic'
 import * as React from 'react'
-import { MobileFallback } from '../src/components/MobileFallback'
-import { UnitHighlight } from '../src/components/UnitHighlight'
-import { WideBlock } from '../src/components/WideBlock'
-import { useTranslation } from '../src/i18n'
+
+import { NextPage } from 'next'
+
+import { Gallery } from '../src/components/Gallery'
+import { Title } from '../src/components/Title'
+import { useLangUrlPrefix } from '../src/hooks/useLangUrlPrefix'
+import { useTranslation, withTranslation } from '../src/i18n'
 
 const Page: NextPage = () => {
     const { t } = useTranslation()
+    const lang = useLangUrlPrefix()
 
     return (
         <>
-            <MobileFallback
-                mobile={(
-                    <img
-                        width={'100%'}
-                        src={'https://raw.githubusercontent.com/tmshv/popcode/master/sketches/unit4_z3/preview.png'}
-                        style={{
-                            marginBottom: 50,
-                        }}
-                    />
-                )}
-            >
-                <WideBlock
-                    style={{
-                        marginBottom: 30,
-                    }}
-                >
-                    <iframe
-                        height={300}
-                        style={{
-                            backgroundColor: 'none',
-                            width: '100%',
-                        }}
-                        scrolling={'no'}
-                        title={'xeIKs'}
-                        src={'//tmshv.github.io/popcode/unit4_z3/'}
-                        frameBorder={'no'}
-                    />
-                </WideBlock>
-            </MobileFallback>
+            <Title>{t('Repository')}</Title>
 
-            <UnitHighlight
+            <Gallery
                 style={{
-                    padding: 'var(--content-padding)',
+                    marginBottom: 50,
                 }}
-                size={'small'}
                 items={[
                     {
-                        date: new Date('2019-11-07'),
-                        href: '/repository/edu_ugra',
-                        text: t('highlight-edu_ugra'),
+                        href: t(`${lang}/repository/volokolamsk`),
+                        src: 'https://unit4.io/files/volokolamsk/04.jpg',
+                        text: t('repository-volokolamsk'),
                     },
                     {
-                        date: new Date('2019-10-01'),
-                        href: '/repository/uray_ppi',
-                        text: t('highlight-uray_ppi'),
+                        href: t(`${lang}/repository/latlng`),
+                        src: 'https://unit4.io/files/latlng/latlng.gif',
+                        text: t('repository-latlng'),
                     },
                     {
-                        date: new Date('2019-08-30'),
-                        href: '/repository/oymyakon',
-                        text: t('highlight-oymyakon'),
+                        href: t(`${lang}/repository/edu_ugra`),
+                        src: 'https://unit4.io/files/edu_ugra/01.jpg',
+                        text: t('repository-edu_ugra'),
                     },
                     {
-                        date: new Date('2019-07-28'),
-                        href: '/repository/delta',
-                        text: t('highlight-delta'),
+                        href: t(`${lang}/repository/uray_ppi`),
+                        src: 'https://unit4.io/files/uray_ppi/urai.jpg',
+                        text: t('repository-uray_ppi'),
+                    },
+                    {
+                        href: t(`${lang}/repository/heterotopia`),
+                        src: 'https://unit4.io/files/heterotopia/heterotopia.gif',
+                        text: t('repository-heterotopia'),
+                    },
+                    {
+                        href: t(`${lang}/repository/oymyakon`),
+                        src: 'https://unit4.io/files/oy/viz_gulag.jpg',
+                        text: t('repository-oymyakon'),
+                    },
+                    {
+                        href: t(`${lang}/repository/hovrinka`),
+                        src: 'https://unit4.io/files/hovrinka/hovrinka.jpg',
+                        text: t('repository-hovrinka'),
+                    },
+                    {
+                        href: t(`${lang}/repository/delta`),
+                        src: 'https://unit4.io/files/delta/delta.jpg',
+                        text: t('repository-delta'),
+                    },
+                    {
+                        href: t(`${lang}/repository/application_manual`),
+                        src: 'https://unit4.io/files/app_manual/manual.jpg',
+                        text: t('repository-application_manual'),
+                    },
+                    {
+                        href: t(`${lang}/repository/gor_projects_spb`),
+                        src: 'https://spb.city4people.ru/static/img_c4p/project3/todo-after_.jpg',
+                        text: t('repository-gor_projects_spb'),
+                    },
+                    {
+                        href: t(`${lang}/repository/yoshkola`),
+                        src: 'https://unit4.io/files/yoshkola/04.jpg',
+                        text: t('repository-yoshkola'),
+                    },
+                    {
+                        href: t(`${lang}/repository/boulevard_scnd`),
+                        src: 'https://unit4.io/files/boulevard_scnd/boulevard.jpg',
+                        text: t('repository-boulevard_scnd'),
+                    },
+                    {
+                        href: t(`${lang}/repository/park_scnd`),
+                        src: 'https://unit4.io/files/park_scnd/park.jpg',
+                        text: t('repository-park_scnd'),
+                    },
+                    {
+                        href: t(`${lang}/repository/shelter`),
+                        src: 'https://unit4.io/files/shelter/01.jpg',
+                        text: t('repository-shelter'),
+                    },
+                    {
+                        href: t(`${lang}/repository/swarm`),
+                        src: 'https://unit4.io/files/swarm/swarm.jpg',
+                        text: t('repository-swarm'),
+                    },
+                    {
+                        href: t(`${lang}/repository/dc_scnd`),
+                        src: 'https://unit4.io/files/dc_scnd/dc.gif',
+                        text: t('repository-dc_scnd'),
+                    },
+                    {
+                        href: t(`${lang}/repository/gb_scnd`),
+                        src: 'https://unit4.io/files/gb_scnd/gb.jpg',
+                        text: t('repository-gb_scnd'),
+                    },
+                    {
+                        href: t(`${lang}/repository/samarapark`),
+                        src: 'https://unit4.io/files/samarapark/10.jpg',
+                        text: t('repository-samarapark'),
+                    },
+                    {
+                        href: t(`${lang}/repository/garagescreen`),
+                        src: 'https://unit4.io/files/garagescreen/02.gif',
+                        text: t('repository-garagescreen'),
+                    },
+                    {
+                        href: t(`${lang}/repository/d_scnd`),
+                        src: 'https://unit4.io/files/d_scnd/03.jpg',
+                        text: t('repository-d_scnd'),
+                    },
+                    {
+                        href: t(`${lang}/repository/krvostok`),
+                        src: 'https://unit4.io/files/krvostok/01.jpg',
+                        text: t('repository-krvostok'),
+                    },
+                    {
+                        href: t(`${lang}/repository/trollgardens`),
+                        src: 'https://unit4.io/files/trollgardens/05.jpg',
+                        text: t('repository-trollgardens'),
+                    },
+                    {
+                        href: t(`${lang}/repository/chistopol2`),
+                        src: 'https://unit4.io/files/chistopol2/07.jpg',
+                        text: t('repository-chistopol2'),
+                    },
+                    {
+                        href: t(`${lang}/repository/kemb`),
+                        src: 'https://unit4.io/files/kemb/01.jpg',
+                        text: t('repository-kemb'),
+                    },
+                    {
+                        href: t(`${lang}/repository/model4`),
+                        src: 'https://unit4.io/files/model4/01.jpg',
+                        text: t('repository-model4'),
+                    },
+                    {
+                        href: t(`${lang}/repository/chistopol`),
+                        src: 'https://unit4.io/files/chistopol/02.jpg',
+                        text: t('repository-chistopol'),
                     },
                 ]}
             />
@@ -73,4 +156,4 @@ const Page: NextPage = () => {
     )
 }
 
-export default Page
+export default withTranslation('common')(Page)
--- src/server/server.ts
+++ src/server/server.ts
@@ -8,6 +8,63 @@ const port = process.env.PORT || 3000
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const redirectMap = new Map<string, string>([
+    ['/repository/application_manual', '/application-manual'],
+    ['/repository/chistopol', '/chistopol'],
+    ['/repository/chistopol2', '/chistopol2'],
+    ['/repository/delta', '/delta'],
+    ['/repository/edu_ugra', '/ugra-edu'],
+    ['/repository/garagescreen', '/garagescreen'],
+    ['/repository/gor_projects_spb', '/gorprojects-spb'],
+    ['/repository/heterotopia', '/heterotopia'],
+    ['/repository/hovrinka', '/hovrinka'],
+    ['/repository/kemb', '/kemb'],
+    ['/repository/krvostok', '/krvostok'],
+    ['/repository/latlng', '/latlng'],
+    ['/repository/model4', '/model4'],
+    ['/repository/oymyakon', '/oymyakon'],
+    ['/repository/park_scnd', '/scnd-park'],
+    ['/repository/boulevard_scnd', '/scnd-boulevard'],
+    ['/repository/gb_scnd', '/scnd-gb'],
+    ['/repository/d_scnd', '/scnd-d'],
+    ['/repository/dc_scnd', '/scnd-dc'],
+    ['/repository/samarapark', '/samarapark'],
+    ['/repository/shelter', '/shelter'],
+    ['/repository/swarm', '/swarm'],
+    ['/repository/trollgardens', '/trollgardens'],
+    ['/repository/uray_ppi', '/uray-ppi'],
+    ['/repository/volokolamsk', '/volokolamsk'],
+    ['/repository/yoshkola', '/yoshkola'],
+])
+const projects = [
+    '/application-manual',
+    '/chistopol',
+    '/chistopol2',
+    '/delta',
+    '/urga-edu',
+    '/garagescreen',
+    '/gorprojects-spb',
+    '/heterotopia',
+    '/hovrinka',
+    '/kemb',
+    '/krvostok',
+    '/latlng',
+    '/model4',
+    '/oymyakon',
+    '/scnd-park',
+    '/scnd-boulevard',
+    '/scnd-gb',
+    '/scnd-d',
+    '/scnd-dc',
+    '/samarapark',
+    '/shelter',
+    '/swarm',
+    '/trollgardens',
+    '/uray-ppi',
+    '/volokolamsk',
+    '/yoshkola',
+]
+
 function createPageName(name: string, defaultLang: string, lang?: string): string {
     if (!lang || lang === defaultLang) {
         return name
@@ -29,35 +86,40 @@ app.prepare()
 
         server.get('/about', (req, res) => {
             const lang = (req as any).lng
-            const page = createPageName('/about', defaultLang, lang )
+            const page = createPageName('/about', defaultLang, lang)
             app.render(req, res, page, {})
         })
 
         server.get('/partners', (req, res) => {
             const lang = (req as any).lng
-            const page = createPageName('/partners', defaultLang, lang )
+            const page = createPageName('/partners', defaultLang, lang)
             app.render(req, res, page, {})
         })
 
         server.get('/contact', (req, res) => {
             const lang = (req as any).lng
-            const page = createPageName('/contact', defaultLang, lang )
+            const page = createPageName('/contact', defaultLang, lang)
             app.render(req, res, page, {})
         })
 
-        server.get('/repository', (req, res) => {
-            app.render(req, res, '/repository', {})
-        })
-
-        server.get('/repository/:slug', (req, res) => {
-            const lang = (req as any).lng
-            const slug = req.params.slug
-            const actualPage = createPageName(`/repository/${slug}`, defaultLang, lang)
-
-            app.render(req, res, actualPage, {})
-        })
-
         server.get('*', (req, res) => {
+            if (redirectMap.has(req.path)) {
+                console.log('will redirect')
+
+                const to = redirectMap.get(req.path)
+                res.redirect(to)
+                res.end()
+                return
+            }
+
+            if (projects.includes(req.path)) {
+                const lang = (req as any).lng
+                const actualPage = createPageName(`/project${req.path}`, defaultLang, lang)
+
+                app.render(req, res, actualPage, {})
+                return
+            }
+
             return handle(req, res)
         })
 
