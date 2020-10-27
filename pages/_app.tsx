1880a76434681e79715cc8efea64a7b6ed839cc8
Roman Timashev <roman@tmshv.ru>
2020-10-27 20:10:49 +0300

    _document custom changes moved to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { Article } from '@/components/Article'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { LangContext } from '@/context/lang'
+import { YMetrika } from '@/components/YMetrika'
 
 import 'antd/dist/antd.less'
 import '../src/style.css'
@@ -43,6 +44,17 @@ function MyApp({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/static/favicon1.png" type="image/x-icon" />
+
+                <script src={'https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'} type={'text/javascript'} />
+
+                <script src={'https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js'} type={'text/javascript'} />
+                <link rel={'stylesheet'} href={'https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css'} />
+
+                <meta name="yandex-verification" content="9248f3170aa2f950" />
+                <YMetrika
+                    account={'47295549'}
+                />
             </Head>
 
             <LangContext.Provider value={{
