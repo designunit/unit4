f8b770a663afc7e89328ce6fd75dc81fc4929261
Roman Timashev <roman@tmshv.ru>
2020-11-03 02:50:02 +0300

    types moved to its file

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -13,7 +13,7 @@ import { WideBlock } from '@/components/WideBlock'
 import CompareImage from 'react-compare-image'
 import { useRouter } from 'next/router'
 import { Carousel } from '@/components/Carousel'
-import { ImageProps } from '@/types'
+import { ComponentDto, ImageDto, ImageProps } from '@/types'
 
 function getPathParts(path: string): string[] {
     return path
@@ -84,119 +84,6 @@ async function apiGetPaths(): Promise<string[]> {
     }
 }
 
-type ImageFormatDto = {
-    url: string
-    name: string
-    hash: string
-    ext: string
-    mime: string
-    width: number
-    height: number
-    size: number
-    path?: string
-}
-
-type ImageDto = {
-    id: number
-    url: string
-    name: string
-    hash: string
-    ext: string
-    mime: string
-    width: number
-    height: number
-    size: number
-    path?: string
-    previewUrl?: string
-    provider: string
-    provider_metadata?: object
-    created_at: string
-    updated_at: string
-    caption?: string
-    formats: {
-        thumbnail: ImageFormatDto,
-        large: ImageFormatDto,
-        medium: ImageFormatDto,
-        small: ImageFormatDto,
-    }
-}
-
-type ComponentLinkDto = {
-    id: number
-    component: 'unit-4.link'
-    href: string
-    text: string
-    newTab: boolean
-}
-
-type ComponentLinkBlockDto = {
-    id: number
-    component: 'unit-4.link-block'
-    items: ComponentLinkDto[]
-}
-
-type ComponentTextDto = {
-    id: number
-    component: 'unit-4.text'
-    text: string
-}
-
-type ComponentImageDto = {
-    id: number
-    component: 'unit-4.image'
-    image: ImageDto
-    wide: boolean
-}
-
-type ComponentCarouselDto = {
-    id: number
-    component: 'unit-4.carousel'
-    switchType: string
-    wide: boolean
-    media: ImageDto[]
-}
-
-type ComponentTwoImagesDto = {
-    id: number
-    component: 'unit-4.two-images'
-    image1: ImageDto
-    image2: ImageDto
-    ratio: number
-}
-
-type ComponentSwitchImageDto = {
-    id: number
-    component: 'unit-4.switch-image'
-    type: string
-    image1: ImageDto
-    image2: ImageDto
-    label1: string
-    label2: string
-}
-
-type ComponentCompareDto = {
-    id: number
-    component: 'unit-4.compare'
-    before: ImageDto
-    after: ImageDto
-    startRatio: number
-}
-
-type ComponentEmbedDto = {
-    id: number
-    component: 'unit-4.embed'
-    url: string
-}
-
-type ComponentDto =
-    ComponentTextDto
-    | ComponentImageDto
-    | ComponentCarouselDto
-    | ComponentTwoImagesDto
-    | ComponentSwitchImageDto
-    | ComponentEmbedDto
-    | ComponentLinkBlockDto
-    | ComponentCompareDto
 
 async function markdownToHtml(markdown: string) {
     const result = await remark()
