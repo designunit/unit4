952fd9c1c7fc25471a78f8d4201bee444fa9c720
manhir <53478716+manhir@users.noreply.github.com>
2021-09-06 18:45:58 +0300

    upd /index desktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { Gallery } from '../src/components/Gallery'
 import { Title } from '../src/components/Title'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'react-i18next'
+import { TextBlock } from '@/components/TextBlock'
+import Link from 'next/link'
 
 const items = [
     {
@@ -138,22 +140,400 @@ const items = [
     },
 ]
 
+const IndexDoubleBlock = ({ left, right, ...props }) => {
+    return (
+        <div style={{
+            width: '100%',
+            display: 'flex',
+            flexFlow: 'column',
+        }}>
+            <div style={{
+                alignSelf: 'flex-start',
+                display: 'flex',
+                flexFlow: 'column',
+            }}>
+                {left.map(x => (
+                    <div>
+                        {x}
+                    </div>
+                ))}
+            </div>
+            <div style={{
+
+            }}>
+                <div style={{
+                    marginLeft: 'auto',
+                    display: 'flex',
+                    alignSelf: 'flex-end',
+                }}>
+                    <div style={{
+                        flex: '1 0 33%',
+                    }} />
+
+                    <div style={{
+                        marginLeft: 'auto',
+                        display: 'flex',
+                        flexFlow: 'column',
+                    }}>
+                        {right}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+const table = [
+    {
+        name: 'Открытый международном архитектурном конкурсе Москомархитектуры и ГК «А101» на разработку концепции общественных пространств площадью 100 га на территории ЖК «Скандинавия» в Новой Москве',
+        href: '/latlng',
+        status: 'первое место',
+        year: '2017',
+        location: 'Москва',
+    },
+    {
+        name: 'Открытый международном архитектурном конкурсе Москомархитектуры и ГК «А101» на разработку концепции общественных пространств площадью 100 га на территории ЖК «Скандинавия» в Новой Москве',
+        href: '/latlng',
+        status: 'первое место',
+        year: '2017',
+        location: 'Москва',
+    },
+    {
+        name: 'Открытый международном архитектурном конкурсе Москомархитектуры и ГК «А101» на разработку концепции общественных пространств площадью 100 га на территории ЖК «Скандинавия» в Новой Москве',
+        href: '/latlng',
+        status: 'первое место',
+        year: '2017',
+        location: 'Москва',
+    },
+    {
+        name: 'Открытый международном архитектурном конкурсе Москомархитектуры и ГК «А101» на разработку концепции общественных пространств площадью 100 га на территории ЖК «Скандинавия» в Новой Москве',
+        href: '/latlng',
+        status: 'первое место',
+        year: '2017',
+        location: 'Москва',
+    },
+]
+
+const Table = () => (
+    <div style={{
+        display: 'flex',
+        flexFlow: 'column',
+    }}>
+        {table.map(x => (
+            <div style={{
+                display: 'flex',
+                flexFlow: 'nowrap',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <Link
+                    href={x.href}
+                >
+                    <a style={{
+                        padding: '8px 0',
+                        flex: '1 0 50%',
+                    }}>
+                        {x.name}
+                    </a>
+                </Link>
+                <div style={{
+                    width: 0,
+                    margin: '0 16px',
+                    borderRight: 'solid 1px var(--color-background-opposite)',
+                    alignSelf: 'stretch',
+                }} />
+                <div>
+                    {x.status}
+                </div>
+                <div style={{
+                    width: 0,
+                    margin: '0 16px',
+                    borderRight: 'solid 1px var(--color-background-opposite)',
+                    alignSelf: 'stretch',
+                }} />
+                <div>
+                    {x.year}
+                </div>
+                <div style={{
+                    width: 0,
+                    margin: '0 16px',
+                    borderRight: 'solid 1px var(--color-background-opposite)',
+                    alignSelf: 'stretch',
+                }} />
+                <div>
+                    {x.location}
+                </div>
+            </div>
+        ))}
+
+    </div>
+
+)
+
 const Page: NextPage = () => {
     const { t } = useTranslation()
 
+    const vertOffset = '8rem'
+
     return (
         <>
-            <Title>{t('Repository')}</Title>
+            <div style={{
+                position: 'relative',
+                height: '50vh',
+                overflow: 'hidden',
+                marginBottom: vertOffset,
+            }}>
+                <div
+                    style={{
+                        position: 'absolute',
+                        right: 0,
+                        bottom: 0,
+                        zIndex: -1,
+                        background: 'conic-gradient(from 188.75deg at 60.16% 50.19%, rgba(255, 0, 102, 0) -15.49deg, #38DEC2 28.34deg, #0047CB 91.88deg, #D0103A 181.87deg, #FF1B0D 306.52deg, rgba(255, 0, 102, 0) 344.51deg, #38DEC2 388.34deg)',
+                        width: 600,
+                        height: 300,
+                    }}
+                />
+                <h1
+                    style={{
+                        textAlign: 'right',
+                        fontSize: 22,
+                        fontWeight: 400,
+                    }}
+                >
+                    Мы проектная студия design unit 4
+                </h1>
+                <Title
+                    mode='multiline'
+                    caption='Данияр Юсупов, основатель студии'
+                >
+                    <div>
+                        «Знаем силу дизайна
+                    </div>
+                    <div style={{ paddingLeft: '20%' }}>
+                        и щедро делимся ею»
+                    </div>
+                </Title>
+            </div>
 
-            <Gallery
-                style={{
-                    marginBottom: 50,
-                }}
-                items={items.map((x: any) => ({
-                    ...x,
-                    text: t(x.text, { ns: 'projects' }),
-                }))}
-            />
+            <div style={{
+                position: 'relative',
+            }}>
+                <div style={{
+                    position: 'sticky',
+                    top: 0,
+                    zIndex: 2,
+                    display: 'flex',
+                    flexWrap: 'nowrap',
+                    gap: 10,
+                    paddingTop: 10,
+                    marginBottom: '6rem',
+                }}>
+                    {[
+                        {
+                            text: 'СРЕДОВОЕ ПРОЕКТИРОВАНИЕ',
+                            src: '/static/wip.png'
+                        },
+                        {
+                            text: 'ОБРАЗОВАНИЕ',
+                            src: '/static/wip.png'
+                        },
+                        {
+                            text: 'СОФТ',
+                            src: '/static/wip.png'
+                        },
+                    ].map(x => (
+                        <div style={{
+                            position: 'relative',
+                            flex: '1 0 30%',
+                            height: 170,
+                        }}>
+                            <img
+                                src={x.src}
+                                style={{
+                                    display: 'block',
+                                    objectFit: 'cover',
+                                    width: '100%',
+                                    height: '100%',
+                                    filter: '',
+                                }}
+                            />
+                            <div style={{
+                                position: 'absolute',
+                                top: 0,
+                                width: '100%',
+                                height: '100%',
+                                background: '#FF006666',
+                            }} />
+                            <span style={{
+                                position: 'absolute',
+                                zIndex: 2,
+                                top: '50%',
+                                left: '50%',
+                                transform: 'translate(-50%, -50%)',
+                                textAlign: 'center',
+                            }}>
+
+                                {x.text}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+
+                <Title
+                    mode='multiline'
+                >
+                    <div>
+                        Средовое
+                    </div>
+                    <div
+                        style={{
+                            marginBottom: vertOffset,
+                            paddingLeft: '20%',
+                        }}
+                    >
+                        проектирование
+                    </div>
+                </Title>
+
+                <IndexDoubleBlock
+                    left={[
+                        '/ проекты городской среды',
+                        '/ мастерпланы развития территорий',
+                        '/ экспертиза средовых проектов',
+                        '/ разработка дизайн-кода',
+                        '/ проекты образовательных сред',
+                    ]}
+                    right={(
+                        <>
+                            <div>
+                                Сейчас по нашим проектам реализуются центральные пространства в нескольких городах: городской пляж и приморский парк в Дербенте, главная улица и скверы Питкяранты, набережная в Урае, лесопарк в Нягани, центральная улица в Волоколамске, а также территории двух жилых комплексов в Москве.
+                            </div>
+                            <div>
+                                В Чистополе под нашим авторским надзором реализован проект реконструкции бульвара по Президентской программе Республики Татарстан. В результате мы прошли путь от первых скетчей до реализации с разработкой деталей уличного оборудования и уже три года получаем фидбек от проекта, который продолжает развиваться.
+                            </div>
+                        </>
+                    )}
+                />
+
+                <Title
+                    crossed
+                >
+                    <div
+                        style={{
+                            margin: '4rem 0',
+                            marginLeft: '33%'
+                        }}
+                    >
+                        Образование
+                    </div>
+                </Title>
+                <IndexDoubleBlock
+                    left={[
+                        '/ образовательные программы',
+                        '/ профессиональные воркшопы',
+                        '/ тематические экскурсии',
+                        '/ организация цеховых конференций',
+                    ]}
+                    right={(
+                        <>
+                            <div>
+                                Образование во всех смыслах становится базовым компонентом жизни студии: как внутри, так и снаружи. Создание обучающих программ, конференций и воркшопов, экспертное участие в международных грантовых проектах и конкурсах интегрируются в рабочий процесс, становясь его неотъемлемой частью.
+                            </div>
+                        </>
+                    )}
+                />
+                <Title
+                    crossed
+                >
+                    <div
+                        style={{
+                            margin: '4rem 0',
+                            marginLeft: '66%',
+                        }}
+                    >
+                        Софт
+                    </div>
+                </Title>
+                <IndexDoubleBlock
+                    left={[
+                        '/ инструменты городских исследований ',
+                        '/ софт оптимизации процесса проектирования',
+                    ]}
+                    right={(
+                        <>
+                            <div>
+                                В каждым проекте мы стремимся создавать особые фичи для оптимизации процесса проектирования. Такая специфика предприятия позволяет сфокусироваться на деталях дизайн-решений.
+                            </div>
+                        </>
+                    )}
+                />
+
+                <Title
+                    mode='multiline'
+                >
+                    <div
+                        style={{
+                            marginTop: vertOffset,
+                        }}
+                    >
+                        Команда студии
+                    </div>
+                    <div style={{ paddingLeft: '30%' }}>
+                        сформировалась
+                    </div>
+                    <div>
+                        как экосистема с plug-in
+                    </div>
+                    <div style={{ paddingLeft: '50%' }}>
+                        структурой,
+                    </div>
+                    <div>
+                        где инициируются
+                    </div>
+                    <div style={{ paddingLeft: '10%' }}>
+                        и поддерживаются амбиции
+                    </div>
+                    <div style={{ paddingLeft: '50%' }}>
+                        каждого.
+                    </div>
+                </Title>
+                <IndexDoubleBlock
+                    left={[]}
+                    right={(
+                        <>
+                            <div
+                                style={{
+                                    margin: '4rem 0',
+                                }}
+                            >
+                                В своих проектах мы создаем добавленное качество материального окружения. Важно конструировать такие пространства, которые не только поддерживают уже сложившиеся практики, но и инициируют новые.
+                            </div>
+                        </>
+                    )}
+                />
+            </div>
+
+            <Table />
+
+            <Title
+                mode='multiline'
+            >
+                <div
+                    style={{
+                        marginTop: vertOffset,
+                    }}
+                >
+                    Проектируем среды,
+                </div>
+                <div style={{ paddingLeft: '30%' }}>
+                    которые влияют
+                </div>
+                <div style={{ paddingLeft: '10%' }}>
+                    на жизнь людей
+                </div>
+            </Title>
         </>
     )
 }
--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,35 +1,26 @@
 import * as React from 'react'
-
+import s from './index.module.css'
 import Head from 'next/head'
+import cx from 'classnames'
 
 export interface ITitleProps {
     caption?: string
+    mode?: null | 'multiline'
+    crossed?: boolean
+    offsets?: number[]
 }
 
-export const Title: React.FC<ITitleProps> = ({ caption, children }) => (
-    <div className={'title'}>
-        <style jsx>{`
-            .title {
-                margin-bottom: 50px;
-                padding: var(--content-padding);
-            }
-
-            .title-caption {
-                margin: 0;
-
-                line-height: var(--font-second-size);
-            }
-        `}</style>
-
+export const Title: React.FC<ITitleProps> = ({ caption, children, mode, offsets, crossed = false }) => (
+    <div className={s.title}>
         <Head>
             <title>{children}</title>
         </Head>
 
-        <h1>
+        <h1 className={cx(s.h1, mode == 'multiline' && s.multiline, crossed && s.crossed)}>
             {children}
         </h1>
 
-        <p className={'title-caption'}>
+        <p className={cx(s['title-caption'], mode == 'multiline' && s.multilineCaption)}>
             {caption}
         </p>
     </div>
