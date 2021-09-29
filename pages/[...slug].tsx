84c33a9168caf1e28744aeb7d95c25eaefecff34
manhir <53478716+manhir@users.noreply.github.com>
2021-09-29 17:56:44 +0300

    basic setup /[..slug]

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,215 +1,24 @@
-import { ImageSet } from '@/components/ImageSet'
-import Image from 'next/image'
 import { Title } from '@/components/Title'
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import ErrorPage from 'next/error'
-import remark from 'remark'
-import html from 'remark-html'
 import { Meta } from '@/components/Meta'
-import { SwitchImage } from '@/components/SwitchImage'
-import { Embed } from '@/components/Embed'
-import { UnitHighlight } from '@/components/UnitHighlight'
-import { WideBlock } from '@/components/WideBlock'
-import CompareImage from 'react-compare-image'
 import { useRouter } from 'next/router'
-import { Carousel } from '@/components/Carousel'
-import { ComponentDto, ImageDto, ImageProps } from '@/types'
+import { PageDefinition } from '@/types'
+import { getPageBySlug, getPages } from './api'
+import { serialize } from 'next-mdx-remote/serialize'
+import React from 'react'
+import { MDXRemote } from 'next-mdx-remote'
+import { Image } from '@/components/Image'
+import { UnitHighlight } from '@/components/UnitHighlight'
+import { Carousel } from 'antd'
 
-function getPathParts(path: string): string[] {
-    return path
-        .split('/')
-        .filter(Boolean)
+const mdxComponents = {
+    Image,
+    Carousel,
+    UnitHighlight
 }
 
-async function apiGetLinkBlock(id: number, locale: string): Promise<any> {
-    const url = `https://unit.tmshv.com/unit-4-link-blocks/${id}`
-    try {
-        const res = await fetch(url)
-        if (!res.ok) {
-            return null
-        }
-        let data = await res.json()
-        if (data.length === 0) {
-            return null
-        }
-
-        return data.items.map((item, i) => ({
-            // "__component": "unit-4.link",
-            component: item.__component,
-            href: item.href,
-            id: i,
-            newTab: item.newTab ?? false,
-            text: getLocalizedValue(item, locale, 'text'),
-        }))
-    } catch (error) {
-        return null
-    }
-}
-
-async function apiGetPage(slug: string, locale: string): Promise<any> {
-    const url = `https://unit.tmshv.com/unit-4-pages?slug=${slug}`
-    try {
-        const res = await fetch(url)
-        if (!res.ok) {
-            return null
-        }
-        let data = await res.json()
-        if (data.length === 0) {
-            return null
-        }
-
-        return getPage(data[0], locale)
-    } catch (error) {
-        return null
-    }
-}
-
-async function apiGetPaths(): Promise<string[]> {
-    const url = `https://unit.tmshv.com/unit-4-pages/paths`
-
-    try {
-        const res = await fetch(url)
-        if (!res.ok) {
-            return []
-        }
-
-        let data = await res.json()
-        if (data.length === 0) {
-            return []
-        }
-
-        return data.map((x: any) => x.slug)
-    } catch (error) {
-        return []
-    }
-}
-
-
-async function markdownToHtml(markdown: string) {
-    const result = await remark()
-        .use(html)
-        .process(markdown)
-    return result.toString()
-}
-
-function one<T>(param: T | T[]): T {
-    if (Array.isArray(param)) {
-        return param[0]
-    }
-
-    return param
-}
-
-function ensureSlash(value: string): string {
-    if (value.charAt(0) === '/') {
-        return value
-    }
-
-    return `/${value}`
-}
-
-async function getPage(data: any, locale: string) {
-    let { content, excerpt, cover, slug } = data
-    cover = one(cover)
-    const title = getLocalizedValue(data, locale, 'title')
-    content = await getPageContent(content, locale)
-
-    return {
-        content,
-        excerpt,
-        title,
-        cover,
-    }
-}
-
-async function getPageContent(content: any[], locale): Promise<ComponentDto[]> {
-    return Promise.all(content.map(async item => {
-        const { __component: component } = item
-
-        switch (component) {
-            case 'unit-4.text': {
-                let text = getLocalizedValue(item, locale, 'text')
-                text = await markdownToHtml(text)
-
-                return {
-                    component,
-                    text,
-                }
-            }
-
-            case 'unit-4.switch-image': {
-                let label1 = getLocalizedValue(item, locale, 'label1')
-                let label2 = getLocalizedValue(item, locale, 'label2')
-
-                return {
-                    component,
-                    label1,
-                    label2,
-                    image1: item.image1,
-                    image2: item.image2,
-                }
-            }
-
-            case 'unit-4.carousel': {
-                if (item.media.length === 1) {
-                    return {
-                        component: 'unit-4.image',
-                        image: item.media[0],
-                        wide: item.wide,
-                    }
-                }
-
-                return {
-                    component,
-                    ...item,
-                }
-            }
-
-            case 'unit-4.two-images': {
-                return {
-                    component,
-                    image1: item.image1,
-                    image2: item.image2,
-                    ratio: item.ratio ?? 0.5,
-                }
-            }
-
-            case 'unit-4.link-block': {
-                let items = await apiGetLinkBlock(item.block.id, locale)
-                items = items ?? []
-                return {
-                    component,
-                    items,
-                }
-            }
-
-            default:
-                return {
-                    component,
-                    ...item,
-                }
-        }
-    }))
-}
-
-function getLocalizedValue(data: any, locale: string, field: string): any {
-    let key = field
-    if (locale === 'en') {
-        key = `${field}_en`
-    }
-
-    return data[key]
-}
-
-type Props = {
-    title: string
-    excerpt: string
-    data: any
-    cover: ImageDto
-    slug: string
-}
-
-const Page: NextPage<Props> = props => {
+const Page: NextPage<PageDefinition> = props => {
     const router = useRouter()
     if (router.isFallback) {
         return (
@@ -217,212 +26,70 @@ const Page: NextPage<Props> = props => {
         )
     }
 
-    if (!props.data) {
+    if (!props.source) {
         return (
             <ErrorPage statusCode={404} />
         )
     }
 
-    const url = `https://unit4.io${props.slug}`
-    const coverUrl = props.cover?.formats?.large?.url
-
     return (
         <div>
             <article>
-                <Title>{props.title}</Title>
+                <Title>{props.excerpt}</Title>
                 <Meta
-                    title={props.title}
-                    description={props.excerpt}
-                    image={coverUrl}
-                    url={url}
+                    title={props.excerpt}
+                    description={null}
+                    image={null}
+                    url={`https://unit4.io${props.slug}`}
                 />
 
-                {props.data.content.map((item: ComponentDto, i) => {
-                    const id = i // todo plz use item.id
-
-                    if (item.component === 'unit-4.text') {
-                        return (
-                            <div
-                                key={id}
-                                className={'article-fragment'}
-                                dangerouslySetInnerHTML={{ __html: item.text }} style={{
-                                    // marginBottom: '16px',
-                                }}
-                            />
-                        )
-                    }
-
-                    if (item.component === 'unit-4.image') {
-                        const img = item.image
-                        const content = (
-                            <Image
-                                key={id}
-                                src={img.url}
-                                width={img.width}
-                                height={img.height}
-                                quality={85}
-                            />
-                        )
-                        return !item.wide ? content : (
-                            <WideBlock>
-                                {content}
-                            </WideBlock>
-                        )
-                    }
-
-                    if (item.component === 'unit-4.embed') {
-                        return (
-                            <Embed
-                                key={id}
-                                src={item.url}
-                            />
-                        )
-                    }
-
-                    if (item.component === 'unit-4.carousel') {
-                        const items = item.media.map(item => ({
-                            src: item.url,
-                            width: item.width,
-                            height: item.height,
-                        }))
-
-                        return (
-                            <Carousel
-                                key={id}
-                                images={items}
-                                transition={item.switchType as any}
-                            />
-                        )
-                    }
-
-                    if (item.component === 'unit-4.two-images') {
-                        const images: ImageProps[] = [item.image1, item.image2,]
-                            .map(x => ({
-                                src: x.url,
-                                width: x.width,
-                                height: x.height,
-                            }))
-                        const l = Math.round(24 * item.ratio)
-                        const r = 24 - l
-                        const layout = {
-                            span: [l, r],
-                        }
-
-                        return (
-                            <ImageSet
-                                key={id}
-                                items={images}
-                                layout={layout}
-                            />
-                        )
-                    }
-
-                    if (item.component === 'unit-4.compare') {
-                        return (
-                            <CompareImage
-                                key={id}
-                                sliderPositionPercentage={0.75}
-                                leftImage={item.after.url}
-                                rightImage={item.before.url}
-                            />
-                        )
-                    }
-
-                    if (item.component === 'unit-4.link-block') {
-                        return (
-                            <UnitHighlight
-                                items={item.items}
-                            />
-                        )
-                    }
-
-                    if (item.component === 'unit-4.switch-image') {
-                        const links = [
-                            {
-                                src: item.image1.url,
-                                width: item.image1.width,
-                                height: item.image1.height,
-                                buttonText: item.label1,
-                            },
-                            {
-                                src: item.image2.url,
-                                width: item.image2.width,
-                                height: item.image2.height,
-                                buttonText: item.label2,
-                            },
-                        ]
-                        return (
-                            <SwitchImage
-                                images={links}
-                            />
-                        )
-                    }
-
-                    return (
-                        <pre key={id}>
-                            {JSON.stringify(item, null, 4)}
-                        </pre>
-                    )
-                })}
+                <MDXRemote {...props.source} components={mdxComponents} />
             </article>
-
-            {/* <pre>
-                {JSON.stringify(props, null, 4)}
-            </pre> */}
         </div>
     )
 }
 
-export const getStaticProps: GetStaticProps<Props> = async ctx => {
-    const locale = ctx.locale
-    let slug = ctx.params.slug
-    if (!slug) {
-        return {
-            props: {
-                title: '',
-                data: null,
-                excerpt: null,
-                slug: null,
-                cover: null,
-            }
-        }
+export const getStaticProps: GetStaticProps<PageDefinition> = async ctx => {
+    let slug = ''
+    if (ctx.params) {
+        slug = (ctx.params!.slug! as string[]).join('/')
     }
-    slug = ensureSlash(one(slug))
+    slug = '/' + slug
 
-    const data = await apiGetPage(slug, locale)
-    if (!data) {
+    const page = await getPageBySlug(ctx.locale, slug)
+
+    if (!page) {
         return {
-            props: {
-                title: '',
-                data: null,
-                excerpt: null,
-                slug,
-                cover: null,
-            }
+            notFound: true,
         }
     }
+
+    const { content, ...def } = page
+    const source = await serialize(content)
 
     return {
         revalidate: 30,
         props: {
-            data,
-            title: data.title,
-            excerpt: data.excerpt,
-            slug,
-            cover: data.cover,
+            ...def,
+            source,
         }
     }
 }
 
 export const getStaticPaths: GetStaticPaths = async ctx => {
-    const paths = await apiGetPaths()
+    const pages = await getPages()
+
     return {
+        paths: pages
+            .map(({ slug, locale }) => {
+                const slugNoSlash = slug.split('/').slice(1)
+
+                return {
+                    params: { slug: slugNoSlash },
+                    locale,
+                }
+            }),
         fallback: true,
-        paths: paths.map(slug => ({
-            params: {
-                slug: getPathParts(slug),
-            },
-        })),
     }
 }
 
