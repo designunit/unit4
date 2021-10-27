b1168e37d04401f20ddc6f94fabcd37e5934771d
manhir <53478716+manhir@users.noreply.github.com>
2021-10-27 17:37:20 +0300

    new mobile menu icons and mobile safari fix

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,9 +4,10 @@ import Media from 'react-media'
 import { Logo } from '../Logo'
 import { IMenuItemProps, MenuItem } from './MenuItem'
 import s from './index.module.css'
-import { Icon } from '@mdi/react'
 import { mdiFacebook, mdiInstagram, mdiVimeo } from '@mdi/js'
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+import Icon from '@mdi/react'
+import { mdiDragHorizontalVariant, mdiClose } from '@mdi/js'
 
 export interface IMenuProps {
     items: IMenuItemProps[]
@@ -43,15 +44,15 @@ export const Menu: React.FC<IMenuProps> = ({ items, vertical = false }) => {
                         <button className={s.mobileButton}
                             onClick={() => setMobileOpen(!mobileOpen)}
                         >
-                            <svg viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ opacity: mobileOpen ? 0 : 1 }} >
-                                <path d="M12 5L36 5L36 9.5L12 9.5L12 5ZM2.59078e-06 16.25L36 16.25L36 20.75L2.94047e-06 20.75L2.59078e-06 16.25ZM12 27.5L36 27.5L36 32L12 32L12 27.5Z"
-                                    fill="var(--color-background-opposite)"
-                                />
-                            </svg>
-                            <svg viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ opacity: mobileOpen ? 1 : 0 }} >
-                                <path d="M0.362179 26.2739L25.818 0.818019L29 4L16.2721 16.7279L3.54416 29.4558L0.362179 26.2739Z" fill="var(--color-background)" />
-                                <path d="M25.456 29.6378L0.000148177 4.18198L3.18213 1L15.91 13.7279L28.638 26.4558L25.456 29.6378Z" fill="var(--color-background)" />
-                            </svg>
+                            <Icon
+                                path={mdiDragHorizontalVariant}
+                                style={{ opacity: mobileOpen ? 0 : 1, fill: 'var(--color-background-opposite)' }}
+                            />
+                            <Icon
+                                path={mdiClose}
+                                className={s.cross}
+                                style={{ opacity: mobileOpen ? 1 : 0 }}
+                            />
                         </button>
 
                         <div className={s.mobileMenuAnchor}>
