6d6ac768cb3b07e6d76edb6e5ac6a7c12a473ecf
manhir <53478716+manhir@users.noreply.github.com>
2022-01-17 18:28:00 +0300

    upd Flex mobile styles

--- src/components/Flex/index.tsx
+++ src/components/Flex/index.tsx
@@ -2,9 +2,10 @@ import classNames from 'classnames'
 import React from 'react'
 import s from './index.module.css'
 
-export const Flex: React.FC<any> = ({ children, col }) => {
+export const Flex: React.FC<any> = ({ children, col, style }) => {
     return (
         <div
+            style={style}
             className={classNames(s.container, col && s.col)}
         >
             {children}
