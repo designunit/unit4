d3bf27011e8f1750592a7265757e7965a11f7599
manhir <53478716+manhir@users.noreply.github.com>
2022-01-19 14:22:52 +0300

    upd article width

--- src/components/WideBlock/index.tsx
+++ src/components/WideBlock/index.tsx
@@ -1,20 +1,15 @@
 import * as React from 'react'
+import s from './index.module.css'
 
 export interface IWideBlockProps {
     style?: React.CSSProperties
 }
 
 export const WideBlock: React.FC<IWideBlockProps> = props => (
-    <div style={props.style}>
-        <style jsx>{`
-            div {
-                position: relative;
-                width: 100vw;
-                margin-left: -50vw;
-                left: 50%;
-            }
-        `}</style>
-
+    <div
+        className={s.container}
+        style={props.style}
+    >
         {props.children}
     </div>
 )
