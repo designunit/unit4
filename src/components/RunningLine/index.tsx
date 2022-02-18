e9395de17e4e704463ff3359b2b56d2bf2345d7e
manhir <53478716+manhir@users.noreply.github.com>
2022-02-18 18:48:35 +0300

    moved RunningLine to separate component

--- src/components/RunningLine/index.tsx
+++ src/components/RunningLine/index.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames'
 import React, { CSSProperties } from 'react'
 import s from './runningLine.module.css'
 
-export const RunningLine: React.FC<{ style?: CSSProperties, direction?: 'left' | 'right', speed?: string }> = ({ children, style, direction = 'left', speed = '20s' }) => {
+export const RunningLine: React.FC<{ style?: CSSProperties, direction?: 'left' | 'right' }> = ({ children, style, direction = 'left' }) => {
     return (
         <div className={classNames(s.container, direction == 'left' && s.left, direction == 'right' && s.right)} style={style} >
             {children}
