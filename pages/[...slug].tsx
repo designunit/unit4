c2632bd6d27ecff61384d871391d82fe89d007ee
Roman Timashev <roman@tmshv.ru>
2022-03-12 17:10:56 +0300

    Load DatavizStage component in dynamic

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -9,7 +9,6 @@ import React from 'react'
 import { MDXRemote } from 'next-mdx-remote'
 import { Image } from '@/components/Image'
 import { UnitHighlight } from '@/components/UnitHighlight'
-import DatavizStage from '@/app/oymyakon/DatavizStage'
 import { ImageSet } from '@/components/ImageSet'
 import { WideBlock } from '@/components/WideBlock'
 import { HeterotopiaTitle } from '@/app/heterotopia/HeterotopiaTitle'
@@ -19,6 +18,9 @@ import { Flex } from '@/components/Flex'
 import { BlackHighlight } from '@/components/BlackHighlight'
 import { BeforeAfter } from '@/components/BeforeAfter'
 import { Carousel } from '@/components/Carousel'
+import dynamic from 'next/dynamic'
+
+const DatavizStage = dynamic(import('@/app/oymyakon/DatavizStage'), { ssr: false })
 
 const mdxComponents = {
     Image,
