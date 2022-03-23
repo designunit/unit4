b581a47717cc70489151c82c389516ec994749ba
manhir <53478716+manhir@users.noreply.github.com>
2022-03-23 15:15:17 +0300

    upd swiper navigation

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,12 +1,11 @@
 import { ImageProps } from '@/types'
 import s from './index.module.css'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Autoplay, Navigation, Pagination } from 'swiper'
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
+import { Autoplay, Pagination } from 'swiper'
 import React from 'react'
 
 import 'swiper/css'
 import 'swiper/css/autoplay'
-import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
 export type CarouselProps = {
@@ -15,21 +14,39 @@ export type CarouselProps = {
     style?: React.CSSProperties
 }
 
-export const Carousel: React.FC<CarouselProps> = ({ children }) => (
-    <Swiper
-        modules={[Autoplay, Navigation, Pagination]}
-        autoplay={{ delay: 7000 }}
-        pagination={{
-            bulletClass: `swiper-pagination-bullet ${s.bullet}`,
-            clickable: true,
-        }}
-        navigation
-        loop
-    >
-        {React.Children.map(children, (child, i) => (
-            <SwiperSlide key={i}>
-                {child}
-            </SwiperSlide>
-        ))}
-    </Swiper >
-)
+const Arrows = () => {
+    const swiper = useSwiper()
+    return (
+        <>
+            <div
+                className={s.left}
+                onClick={() => swiper.slidePrev()}
+            />
+            <div
+                className={s.right}
+                onClick={() => swiper.slideNext()}
+            />
+        </>
+    )
+}
+
+export const Carousel: React.FC<CarouselProps> = ({ children }) => {
+    return (
+        <Swiper
+            modules={[Autoplay, Pagination]}
+            autoplay={{ delay: 7000 }}
+            pagination={{
+                bulletClass: s.bullet,
+                clickable: true,
+            }}
+            loop
+        >
+            <Arrows />
+            {React.Children.map(children, (child, i) => (
+                <SwiperSlide key={i}>
+                    {child}
+                </SwiperSlide>
+            ))}
+        </Swiper >
+    )
+}
