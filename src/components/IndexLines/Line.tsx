40d9e3ece121be3e23c1b91c716da682f0c5441e
Roman Timashev <roman@tmshv.ru>
2023-02-03 19:04:12 +0600

    Line component refactoring

--- src/components/IndexLines/Line.tsx
+++ src/components/IndexLines/Line.tsx
@@ -1,25 +1,36 @@
-export const Line = ({ line }) => line.map((x, i) => (
+import { Fragment } from 'react'
+
+export type LineProps = {
+    line: string[]
+}
+
+// TODO: shitty
+export const Line: React.FC<LineProps> = ({ line }) => (
     <>
-        <span
-            style={{
-                textTransform: x == 'design : : unit' ? 'lowercase' : 'uppercase',
-                fontWeight: 'bold',
-                fontSize: 42,
-                color: x == 'design : : unit' ? 'black' : 'lightgray',
-                marginRight: '1rem',
-            }}
-        >
-            {x}
-        </span>
-        <span
-            style={{
-                fontSize: 42,
-                fontWeight: 'bold',
-                color: 'lightgray',
-                marginRight: '1rem',
-            }}
-        >
-            /
-        </span>
+        {line.map((line, i) => (
+            <Fragment key={i}>
+                <span
+                    style={{
+                        textTransform: line == 'design : : unit' ? 'lowercase' : 'uppercase',
+                        fontWeight: 'bold',
+                        fontSize: 42,
+                        color: line == 'design : : unit' ? 'black' : 'lightgray',
+                        marginRight: '1rem',
+                    }}
+                >
+                    {line}
+                </span>
+                <span
+                    style={{
+                        fontSize: 42,
+                        fontWeight: 'bold',
+                        color: 'lightgray',
+                        marginRight: '1rem',
+                    }}
+                >
+                    /
+                </span>
+            </Fragment>
+        ))}
     </>
-))
+)
