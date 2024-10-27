e630bb0e75e87049ba7c361b537a02b942dbd5a6
Roman Timashev <roman@tmshv.ru>
2024-10-28 02:57:47 +0400

    text block component refactoring

--- src/components/TextBlock/index.tsx
+++ src/components/TextBlock/index.tsx
@@ -1,32 +1,11 @@
+import s from './text.module.css'
+
 export type TextBlockProps = {
     children: React.ReactNode
 }
 
 export const TextBlock: React.FC<TextBlockProps> = ({ children }) => (
-    <div>
-        <style jsx>{`
-            div {
-                --text-block-width: 50%;
-
-                min-width: 300px;
-                width: var(--text-block-width);
-                hyphens: auto;
-                margin-bottom: 1em;
-            }
-
-            @media screen and (max-width: 1280px) {
-                div {
-                    --text-block-width: 40%;
-                }
-            }
-
-            @media screen and (max-width: 31.25em) {
-                div {
-                    --text-block-width: 100%;
-                }
-            }
-        `}</style>
-
+    <div className={s.block}>
         {children}
     </div>
 )
