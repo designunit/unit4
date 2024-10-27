671737da02b81920839df9a3ede3f06c91f839c3
Roman Timashev <roman@tmshv.ru>
2024-10-28 02:24:09 +0400

    23 -> 24

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { LangButton } from '../LangButton'
 
 export const Footer: React.FC = () => (
     <footer className={s.footer}>
-        <section>© 2017-2023 design : : unit 4</section>
+        <section>© 2017-2024 design : : unit 4</section>
         <section className={'right'}>
             <a
                 href='mailto:inbox@unit4.io'
