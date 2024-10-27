b11343fa911999b83d01bd9f3a382acb9d85ecd5
Roman Timashev <roman@tmshv.ru>
2024-10-28 00:15:00 +0400

    update carousel: swiper 11; update arrows; refactor component

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,25 +1,43 @@
 import type { ImageProps } from '@/types'
-import s from './index.module.css'
+import s from './carousel.module.css'
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
-import { Autoplay, Pagination } from 'swiper'
+import { Autoplay, Pagination } from 'swiper/modules'
 import React from 'react'
 
 import 'swiper/css'
 import 'swiper/css/autoplay'
 import 'swiper/css/pagination'
 
+type ArrowProps = {
+    color: string
+}
+
+const Arrow: React.FC<ArrowProps> = ({ color }) => (
+    <svg width="60" height="37" viewBox="0 0 120 74" xmlns="http://www.w3.org/2000/svg">
+        <path
+            fill={color}
+            stroke="none"
+            d="M 112.643669 44.125885 L 119.769554 37 L 83 0.230446 L 75.874123 7.356331 L 100.517784 32 L 0 32 L 0 42 L 100.517784 42 L 75.874123 66.643669 L 83 73.769554 L 112.643669 44.125885 Z"
+        />
+    </svg>
+)
+
 const Arrows: React.FC = () => {
     const swiper = useSwiper()
     return (
         <>
-            <div
-                className={s.left}
+            <button
+                className={`${s.arrow} ${s.left}`}
                 onClick={() => swiper.slidePrev()}
-            />
-            <div
-                className={s.right}
+            >
+                <Arrow color="white" />
+            </button>
+            <button
+                className={`${s.arrow} ${s.right}`}
                 onClick={() => swiper.slideNext()}
-            />
+            >
+                <Arrow color="white" />
+            </button>
         </>
     )
 }
@@ -33,21 +51,24 @@ export type CarouselProps = {
 
 export const Carousel: React.FC<CarouselProps> = ({ children }) => {
     return (
-        <Swiper
-            modules={[Autoplay, Pagination]}
-            autoplay={{ delay: 7000 }}
-            pagination={{
-                bulletClass: `swiper-pagination-bullet ${s.bullet}`,
-                clickable: true,
-            }}
-            loop
-        >
-            <Arrows />
-            {React.Children.map(children, (child, i) => (
-                <SwiperSlide key={i}>
-                    {child}
-                </SwiperSlide>
-            ))}
-        </Swiper>
+        <>
+            <Swiper
+                modules={[Autoplay, Pagination]}
+                autoplay={{ delay: 7000 }}
+                pagination={{
+                    bulletClass: `${s.bullet} swiper-pagination-bullet`,
+                    // bulletClass: s.bullet,
+                    clickable: true,
+                }}
+                loop
+            >
+                {React.Children.map(children, (child, i) => (
+                    <SwiperSlide key={i}>
+                        {child}
+                    </SwiperSlide>
+                ))}
+                <Arrows />
+            </Swiper>
+        </>
     )
 }
