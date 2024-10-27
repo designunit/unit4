a667a4d12e67a0ebd1a4526c92bcc1df0adfc528
Roman Timashev <roman@tmshv.ru>
2024-10-28 02:59:43 +0400

    menu refactoring WIP: disable mobile menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,101 +1,31 @@
-import { useState, useEffect } from 'react'
-import Media from 'react-media'
-import { Logo } from '../Logo'
+'use client'
+
+import { Logo } from '@/components/Logo'
 import { MenuItem } from './MenuItem'
 import type { IMenuItemProps } from './MenuItem'
-import s from './index.module.css'
-import Icon from '@mdi/react'
-import { mdiDragHorizontalVariant, mdiClose } from '@mdi/js'
 
-export interface IMenuProps {
+import s from './menu.module.css'
+
+export type MenuProps = {
     items: IMenuItemProps[]
     vertical?: boolean
 }
 
-export const Menu: React.FC<IMenuProps> = ({ items, vertical = false }) => {
-    const [mobileOpen, setMobileOpen] = useState(false)
-
-    useEffect(() => {
-        if (!document) {
-            return
-        }
-
-        document.body.style.touchAction = mobileOpen ? 'none' : 'auto'
-    }, [mobileOpen])
-
+export const Menu: React.FC<MenuProps> = ({ items, vertical = false }) => {
     return (
         <>
-            <div onClick={() => setMobileOpen(false)}>
-                <Logo
-                    href={'/'}
-                    invert={mobileOpen}
-                />
-            </div>
-
-            <Media queries={{
-                mobile: '(max-width: 31.25em)',
-            }}>
-                {matches => matches.mobile ? (
-                    <>
-                        <button className={s.mobileButton}
-                            onClick={() => setMobileOpen(!mobileOpen)}
-                        >
-                            <Icon
-                                path={mdiDragHorizontalVariant}
-                                style={{ opacity: mobileOpen ? 0 : 1, fill: 'var(--color-background-opposite)' }}
-                            />
-                            <Icon
-                                path={mdiClose}
-                                className={s.cross}
-                                style={{ opacity: mobileOpen ? 1 : 0 }}
-                            />
-                        </button>
-
-                        <div className={s.mobileMenuContainer}
-                            style={{
-                                left: mobileOpen ? 0 : '100vw',
-                            }}
-                        >
-                            <menu className={`${s.menu} ${s.mobileMenu}`}>
-                                {items.map((x, i) => (
-                                    <MenuItem
-                                        key={i}
-                                        {...x}
-                                        onClick={() => setMobileOpen(false)}
-                                    />
-                                ))}
-                            </menu>
-                            <div className={s.contacts}>
-                                <div className={s.icons}>
-                                </div>
-                                <div className={s.links} >
-                                    <a
-                                        href='mailto:inbox@unit4.io'
-                                        target={'_blank'} rel="noreferrer"
-                                    >
-                                        inbox@unit4.io
-                                    </a>
-                                    <a
-                                        href='tel:+79219980303'
-                                        target={'_blank'} rel="noreferrer"
-                                    >
-                                        +7 921 9980303
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </>
-                ) : (
-                    <menu className={`${s.menu} ${vertical ? s.vertical : ''}`}>
-                        {items.map((x, i) => (
-                            <MenuItem
-                                key={i}
-                                {...x}
-                            />
-                        ))}
-                    </menu>
-                )}
-            </Media>
+            <Logo
+                href={'/'}
+                invert={false}
+            />
+            <menu className={`${s.menu} ${vertical ? s.vertical : ''}`}>
+                {items.map((x, i) => (
+                    <MenuItem
+                        key={i}
+                        {...x}
+                    />
+                ))}
+            </menu>
         </>
     )
 }
