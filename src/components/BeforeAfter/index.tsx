b07aaab94b42b7674ebbe7fef8d7248152463a88
Roman Timashev <roman@tmshv.ru>
2024-11-19 16:22:08 +0300

    before-after component refactored: bump react-image-compare, improve captions

--- src/components/BeforeAfter/index.tsx
+++ src/components/BeforeAfter/index.tsx
@@ -1,19 +1,33 @@
 'use client'
 
 import ReactCompareImage from 'react-compare-image'
+import { Flex } from '@/components/Flex'
+import s from './compare.module.css'
 
 export type BeforeAfterProps = {
     before: string
+    beforeCaption?: React.ReactNode
     after: string
+    afterCaption?: React.ReactNode
 }
 
-export const BeforeAfter: React.FC<BeforeAfterProps> = ({ before, after, ...props }) => {
+export const BeforeAfter: React.FC<BeforeAfterProps> = ({ before, after, beforeCaption, afterCaption, ...props }) => {
     return (
-        <ReactCompareImage
-            {...props}
-            leftImage={before}
-            rightImage={after}
-            aspectRatio='wider'
-        />
+        <figure className={s.fig}>
+            <ReactCompareImage
+                {...props}
+                leftImage={before}
+                rightImage={after}
+                aspectRatio='wider'
+            />
+            <div className={s.cap}>
+                <figcaption className={s.left}>
+                    {beforeCaption}
+                </figcaption>
+                <figcaption className={s.right}>
+                    {afterCaption}
+                </figcaption>
+            </div>
+        </figure>
     )
 }
