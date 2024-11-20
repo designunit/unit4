1254ccd134681af598872787c76f305f454382ad
Roman Timashev <roman@tmshv.ru>
2024-11-21 02:05:54 +0300

    clean

--- src/special/oymyakon/Sankey.tsx
+++ src/special/oymyakon/Sankey.tsx
@@ -25,7 +25,6 @@ interface ISankeyProps {
 }
 
 const NivoSankey = ResponsiveSankey as any
-// const Sankey = ResponsiveSankey
 
 export const Sankey: React.FC<ISankeyProps> = ({
     layout = 'horizontal',
