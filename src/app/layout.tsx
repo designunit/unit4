4cda837bb9161330dd67dfcf15cf992123fdc818
Roman Timashev <roman@tmshv.ru>
2024-11-20 13:45:39 +0300

    rm meta viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,10 +32,6 @@ export default async function RootLayout({
     return (
         <html lang="ru">
             <head>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
                 <link
                     rel="shortcut icon"
                     href="/static/favicon.png"
