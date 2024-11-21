64bd462e2fab4bfab08bd9dbb0306e3f23051008
Roman Timashev <roman@tmshv.ru>
2024-11-21 04:20:12 +0300

    found a way to customize --color-background variable

---
+++ src/app/[slug]/layout.tsx
@@ -0,0 +1,35 @@
+import { getPageBySlug } from '@/api'
+import { THEME_COLOR } from '@/constants'
+import type { Viewport } from 'next'
+
+type Params = Promise<{ slug: string }>
+
+export async function generateViewport({ params }: { params: Params}): Promise<Viewport> {
+    const { slug } = await params
+    const page = getPageBySlug('ru', slug)
+    const themeColor = page?.themeColor ?? THEME_COLOR
+    return {
+        themeColor,
+    }
+}
+
+export default async function SlugLayout({ children, params }: { children: React.ReactNode, params: Params }) {
+    const { slug } = await params
+    const page = getPageBySlug('ru', slug)
+    const themeColor = page?.themeColor
+
+    return (
+        <>
+            {!themeColor ? null : (
+                <style>
+                    {`
+                        :root {
+                            --color-background: ${themeColor};
+                        }
+                    `}
+                </style>
+            )}
+            {children}
+        </>
+    )
+}
