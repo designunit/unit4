4c607971287110cd85744dc91c9553707152e8dc
manhir <53478716+manhir@users.noreply.github.com>
2025-10-06 18:14:33 +0300

    upd partners

--- src/components/GalleryPartners/Item.tsx
+++ src/components/GalleryPartners/Item.tsx
@@ -1,17 +1,11 @@
 import s from './item.module.css'
 import Image from 'next/image'
 
-export type ItemProps = {
-    src: string
-    alt: string
-    href: string
-}
-
-export const Item: React.FC<ItemProps> = ({ href, src, alt }) => (
+export const Item: React.FC<{ src: string }> = ({ src }) => (
     <div className={s.item}>
         <Image
             src={src}
-            alt={alt}
+            alt={''}
             fill
             style={{
                 objectFit: 'contain',
--- src/components/Partners/index.tsx
+++ src/components/Partners/index.tsx
@@ -2,78 +2,25 @@ import { GalleryPartners, Item } from '@/components/GalleryPartners'
 import { Title } from '@/components/Title'
 
 const items = [
-    {
-        src: '/static/partners/urbanica.png',
-        href: 'http://urbanica.spb.ru/',
-    },
-    {
-        src: '/static/partners/mla.png',
-        href: 'https://mlaplus.ru/',
-    },
-    {
-        src: '/static/partners/centr-100.jpg',
-        href: 'https://centeragency.org/ru',
-    },
-    {
-        src: '/static/partners/zeitplan.png',
-        href: 'https://instagram.com/zeitplan_team/',
-    },
-    {
-        src: '/static/partners/kf.png',
-        href: 'https://knightfrank.ru/',
-    },
-    {
-        src: '/static/partners/centr comp-100.jpg',
-        href: 'https://gorsreda86.ugraces.ru/',
-    },
-    {
-        src: '/static/partners/centr myrmansk-100.jpg',
-        href: 'https://instagram.com/centrgoroda51/',
-    },
-    {
-        src: '/static/partners/volokalamsk.png',
-        href: 'https://включиволоколамск.рф/',
-    },
-    {
-        src: '/static/partners/institut nn-100.jpg',
-        href: 'https://irgsno.ru/',
-    },
-    {
-        src: '/static/partners/ispring.png',
-        href: 'https://ispring.ru/',
-    },
-    {
-        src: '/static/partners/kiselev-100.jpg',
-        href: 'http://sk-p.ru/',
-    },
-    {
-        src: '/static/partners/concjung.png',
-        href: 'https://cjungle.com/',
-    },
-    {
-        src: '/static/partners/mishin.png',
-        href: 'https://facebook.com/StudioMishin/',
-    },
-    {
-        src: '/static/partners/epishkola-100.jpg',
-        href: 'http://epischool.ru/',
-    },
-    {
-        src: '/static/partners/knappt.png',
-        href: 'http://knappt.com/',
-    },
-    {
-        src: '/static/partners/8l.png',
-        href: 'http://llllllll.ru/',
-    },
-    {
-        src: '/static/partners/kinglab.png',
-        href: 'https://kinglab.com/',
-    },
-    {
-        src: '/static/partners/institut.png',
-        href: 'https://genplanmos.ru/',
-    },
+    '/static/partners/0.png',
+    '/static/partners/1.svg',
+    '/static/partners/2.png',
+    '/static/partners/3.jpg',
+    '/static/partners/4.svg',
+    '/static/partners/5.svg',
+    '/static/partners/6.svg',
+    '/static/partners/7.png',
+    '/static/partners/8.png',
+    '/static/partners/9.svg',
+    '/static/partners/10.png',
+    '/static/partners/11.svg',
+    '/static/partners/12.svg',
+    '/static/partners/13.svg',
+    '/static/partners/14.svg',
+    '/static/partners/15.svg',
+    '/static/partners/16.svg',
+    '/static/partners/17.svg',
+    '/static/partners/18.svg',
 ]
 
 export const Partners: React.FC = () => (
@@ -87,10 +34,8 @@ export const Partners: React.FC = () => (
         }}>
             {items.map((x, i) => (
                 <Item
-                    key={x.src}
-                    src={x.src}
-                    alt={''}
-                    href={x.href}
+                    key={i}
+                    src={x}
                 />
             ))}
         </GalleryPartners>
